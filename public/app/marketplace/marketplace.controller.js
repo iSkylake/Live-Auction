4bc59b75f08f97a1c6597521bbe387f9b5fe4312
starcraft168 <ericeng168@gmail.com>
2017-01-28 01:40:14 -0800

    add dummy data

--- public/app/marketplace/marketplace.controller.js
+++ public/app/marketplace/marketplace.controller.js
@@ -3,19 +3,43 @@ angular.module('myApp')
 		var marketCtrl = this;
 		console.log("parameter", $stateParams);
 
-		console.log('hey');
+		//////// DUMMY DATA //////////
+		var item1 = {
+			title: 'Lion',
+			owner: 'George',
+			image: 'http://assets.worldwildlife.org/photos/1620/images/carousel_small/bengal-tiger-why-matter_7341043.jpg?1345548942',
+			description: 'This is a Bengal Tiger',
+			startTime: '5AM'
+		}
 
-		marketCtrl.itemList = [];
+		var item2 = {
+			title: 'Lion',
+			owner: 'George',
+			image: 'http://assets.worldwildlife.org/photos/1620/images/carousel_small/bengal-tiger-why-matter_7341043.jpg?1345548942',
+			description: 'This is a Bengal Tiger',
+			startTime: '5AM'
+		}
 
-		// marketCtrl.postItem = function(title, image, description, startTime) {
-		// 	var item = {};
-		// 	item.title = title;
-		// 	item.image = image;
-		// 	item.description = description;
-		// 	item.startTime = startTime;
-		// 	return item;
-		// }
-		
+		var item3 = {
+			title: 'Lion',
+			owner: 'George',
+			image: 'http://assets.worldwildlife.org/photos/1620/images/carousel_small/bengal-tiger-why-matter_7341043.jpg?1345548942',
+			description: 'This is a Bengal Tiger',
+			startTime: '5AM'
+		}
+
+
+		marketCtrl.dummyList = [];
+
+		marketCtrl.dummyList.push(item1);
+		marketCtrl.dummyList.push(item2);
+		marketCtrl.dummyList.push(item3);
+
+
+
+
+
+
 		marketCtrl.itemList = [];
 		marketCtrl.postItem = function(title, owner, image, description, startTime) {
 			marketService.postItem(title, owner, image, description, startTime)
@@ -29,7 +53,7 @@ angular.module('myApp')
 		marketCtrl.getItems = function() {
 			marketService.getItems()
 				.then(function successCallback(response) {
-					console.log('successfully retrieved item item', response);
+					console.log('successfully retrieved item', response);
 					marketCtrl.itemList = response;
 				}, function errorCallback(error) {
 					console.log('failed to retrieve item', error);
