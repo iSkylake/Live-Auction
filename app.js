36a60bc35b8ce56b80d58f8657c3892a00faee98
Llorch <skylakebeta@gmail.com>
2017-01-28 02:06:34 -0800

    Try databse

--- app.js
+++ app.js
@@ -7,11 +7,22 @@ var Auction = require('./model/auction.js'),
 	User = require('./model/user.js'),
 	Bid = require('./model/bid.js');
 
+// var TestDB = require('testDB.js');
 
-mongoose.connect("mongodb://localhost/auction");
+mongoose.connect("mongodb://localhost/Auction");
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended:true}));
+
+// TestDB();
+
+app.get('/AB', function(req, res){
+	res.send(User);
+	// User.find({}, function(err, user){
+	// 	res.send();
+	// 	console.log(user);
+	// });
+});
 
 app.listen(3000, function(){
 	console.log("Server Started");
