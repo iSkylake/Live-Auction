8ad1fbc3febc360c6e016459e7e1d9813104e5b6
Chad Lou <chadlou@yahoo.com>
2017-01-27 22:16:49 -0800

    add bodyparser

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 'use strict'
 
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
+
+app.use(bodyParser());
+
+app.use(express.static(__dirname + '/public'));
 
 app.listen(3000, 'localhost', function() {
     console.log('Express is listening on port 3000')
 });
 
 var auctions = require('./mock/auctions');
-console.log(auctions);
+
 app.get('/auctions', function(req, res)  {
     res.send(auctions);
 })
