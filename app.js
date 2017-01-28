fe6b4f636fda368101c6acd1ec2ff76647ba8b8e
Llorch <skylakebeta@gmail.com>
2017-01-28 01:30:49 -0800

    Fix server

--- app.js
+++ app.js
@@ -6,14 +6,12 @@ var express = require('express'),
 var Auction = require('./model/auction.js'),
 	User = require('./model/user.js'),
 	Bid = require('./model/bid/js');
-	
+
 
 mongoose.connect("mongodb://localhost/auction");
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended:true}));
-
-auction
 
 app.listen(3000, function(){
 	console.log("Server Started");
